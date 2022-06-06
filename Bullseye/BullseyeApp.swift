50fdd44d4e3b406e82012fd4f51b30476bafeb8d
Aiganym <ospan.aiganym@gmail.com>
2022-06-06 16:55:59 +0600

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Айганым Оспан on 06.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
